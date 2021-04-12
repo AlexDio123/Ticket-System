aaf6211c62fa0c057fd5b051d204980bc5f0460c
AlexDio <alexdionicio934@hotmail.com>
2021-04-12 12:37:33 -0400

    ticket views, edit and delete added.

--- tickets/urls.py
+++ tickets/urls.py
@@ -2,5 +2,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.create_ticket, name="ticket_create"),
+    path('', views.index, name="ticket_list"),
+    path('create_ticket/', views.create_ticket, name="ticket_create"),
+    path('update_ticket/<str:pk>/', views.update_ticket, name="ticket_update"),
+    path('delete_ticket/<str:pk>/', views.delete_ticket, name="ticket_delete"),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import TicketForm
+from .models import Ticket
 # Create your views here.
+
+
+def index(request):
+    tickets = Ticket.objects.all()
+    context={"tickets":tickets}
+    return render(request, 'tickets/tickets_list.html', context)
+
 
 def create_ticket(request):
     form = TicketForm()
@@ -10,6 +18,30 @@ def create_ticket(request):
         form = TicketForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('/tickets/')
 
     context = {"form":form,}
     return render(request, "tickets/tickets_add.html", context)
+
+def update_ticket(request, pk):
+
+    ticket = Ticket.objects.get(id=pk)
+    form = TicketForm(instance=ticket)
+    if request.method == "POST":
+        print(request.POST)
+        form = TicketForm(request.POST, instance=ticket)
+        if form.is_valid():
+            form.save()
+            return redirect('/tickets/')
+
+    context = {"form":form,}
+    return render(request, "tickets/tickets_add.html", context)
+
+def delete_ticket(request, pk):
+    ticket = Ticket.objects.get(id=pk)
+    if request.method == "POST":
+        ticket.delete()
+        return redirect('/tickets/')
+    context ={'ticket':ticket}
+    return render(request, 'tickets/tickets_delete.html', context)
+
