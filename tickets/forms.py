ca1f2451a4d61f75a46ee08c67456e2da9619e0f
AlexDio <alexdionicio934@hotmail.com>
2021-04-13 10:09:43 -0400

    translated error messages when creating an end date

--- tickets/forms.py
+++ tickets/forms.py
@@ -19,7 +19,7 @@ class TicketForm(forms.ModelForm):
         start_date = cleaned_data.get("start_date")
         end_date = cleaned_data.get("end_date")
         if end_date < start_date:
-            raise forms.ValidationError("End date should be greater than start date.")
+            raise forms.ValidationError("La fecha final debe ser mayor que la fecha inicial.")
 
 
 class CreateUserForm(UserCreationForm):
