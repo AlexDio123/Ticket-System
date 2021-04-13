cd7e791e243003c8b222987ff29edd6723231d24
AlexDio <alexdionicio934@hotmail.com>
2021-04-13 09:29:42 -0400

    date picker widget added

--- reports/filters.py
+++ reports/filters.py
@@ -1,7 +1,11 @@
 import django_filters
 from tickets.models import Ticket
+from django.forms.widgets import DateTimeInput
+
 
 class TicketFilter(django_filters.FilterSet):
+    start_date = django_filters.DateFilter(widget=DateTimeInput(attrs={'type': 'date'}))
+    end_date = django_filters.DateFilter(widget=DateTimeInput(attrs={'type': 'date'}))
     class Meta:
         model= Ticket
         fields = ["start_date", "end_date", 'status']
--- tickets/forms.py
+++ tickets/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.forms.widgets import DateTimeInput
-
 from django.contrib.auth.models import User
 from .models import Ticket
 from employee.models import Employee
@@ -9,11 +8,11 @@ from employee.models import Employee
 
 
 class TicketForm(forms.ModelForm):
-
+    start_date = forms.DateField(widget=DateTimeInput(attrs={'type': 'date'}))
+    end_date = forms.DateField(widget=DateTimeInput(attrs={'type': 'date'}))
     class Meta:
         model = Ticket
         fields = ['subject','employee','description','status','start_date','end_date']
-
 
     def clean(self):
         cleaned_data = super().clean()
